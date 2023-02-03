f74e366643939793c31add2bd8e25206647a7408
akashk011 <97167146+akashk011@users.noreply.github.com>
2023-02-03 15:00:42 +0500

    sql

--- database/sra_l_b (1).sql
+++ database/sra_l_b (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Jan 27, 2023 at 06:13 AM
+-- Generation Time: Feb 03, 2023 at 09:59 AM
 -- Server version: 5.7.36
 -- PHP Version: 7.3.33
 
@@ -35,13 +35,13 @@ CREATE TABLE IF NOT EXISTS `clients` (
   `cl_county` varchar(500) DEFAULT NULL,
   `cl_eircode` varchar(500) DEFAULT NULL,
   `cl_cont_name` varchar(500) DEFAULT NULL,
-  `cl_gender` varchar(155) DEFAULT NULL,
+  `cl_gender` varchar(155) DEFAULT NULL COMMENT 'M for Male , F for female',
   `cl_cont_email` varchar(500) NOT NULL,
   `cl_cont_pwd` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `cl_cont_phone` varchar(500) DEFAULT NULL,
   `cl_cont_desig` varchar(255) DEFAULT NULL,
   `cl_address` varchar(500) DEFAULT NULL,
-  `cl_status` varchar(12) NOT NULL,
+  `cl_status` varchar(12) NOT NULL COMMENT '1 for active, 0 for disable',
   `cl_created` datetime NOT NULL,
   `cl_updated` datetime NOT NULL,
   PRIMARY KEY (`cl_id`)
@@ -111,7 +111,7 @@ CREATE TABLE IF NOT EXISTS `employee` (
   `emp_passport` varchar(500) DEFAULT NULL,
   `emp_occup_health` varchar(500) DEFAULT NULL,
   `emp_work_permit` varchar(500) DEFAULT NULL,
-  `emp_status` varchar(12) DEFAULT NULL,
+  `emp_status` varchar(12) DEFAULT NULL COMMENT '0 For Disable and 1 for Enable',
   `emp_created` datetime NOT NULL,
   `emp_updated` datetime NOT NULL,
   PRIMARY KEY (`emp_id`)
@@ -122,13 +122,13 @@ CREATE TABLE IF NOT EXISTS `employee` (
 --
 
 INSERT INTO `employee` (`emp_id`, `emp_spec1`, `emp_spec2`, `emp_spec3`, `emp_grade1`, `emp_grade2`, `emp_grade3`, `emp_fname`, `emp_lname`, `emp_email`, `emp_gender`, `emp_pwd`, `emp_phone`, `emp_imcr_no`, `emp_pps_no`, `emp_cv`, `emp_imc_cert`, `emp_gv_cert`, `emp_rec_refer`, `emp_passport`, `emp_occup_health`, `emp_work_permit`, `emp_status`, `emp_created`, `emp_updated`) VALUES
-(1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'shahid@prime.edu.pk', '', '$2y$10$r/.rBm5s1H8g5uR9KeJkS.ryJo6EU.G6Wm3f6xbEoxMkZdtDdobbG', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '1', '2023-01-18 09:58:07', '2023-01-25 13:11:38'),
-(2, '1', '1', '3', '1', '1', '4', 'abc', 'x7HZXbWPkV', 'okasha@gmail.com', 'M', '$2y$10$J9x2v52mwYt/8aKzDFy3wOHY3qsc7bWkQGV/JZcJWq/.ySaJmyfwi', '924524', '714057', 725133, 'TmVEMytzYzJqK05yUWp3N3VzazZkVFdiOEFNSy9GSXRSbWVOSlBYZW5XOD0=', 'ZzE4TGVHYkFNc1o1NlVjQmJadkMzUG5SeVdwMkZOTlhyR2EvNUxZQTZuUT0=', 'VlY1RFpjOFZnSzNEMmZRRE8zb3hhVmVCbStva25FLy9Cc0dqeWtLVFRDQT0=', 'ZzE4TGVHYkFNc1o1NlVjQmJadkMzTjAvekFoRElhYjVDNnFqUllKQXFmWT0=', 'QjlhMENHeURLbGl1T2h4ejF4cEt3QT09', 'QjlhMENHeURLbGl1T2h4ejF4cEt3QT09', 'QjlhMENHeURLbGl1T2h4ejF4cEt3QT09', '1', '2023-01-18 11:35:25', '2023-01-26 18:32:13'),
-(3, 'yJUX7csFaF', 'zcSRZKFIYv', '', 'myKnyGx0l8', 'AKGMjse3UN', '', 'qeq8aHtKCC', 'gRDsECihGB', 'test@pmc.com', '', '$2y$10$m96xc1NSjcLyD0b...VoT.BzXdmJTbhjr.E4/ajbv6R7V92.ptKty', '533106', '224990', 132701, 'bW9OMGIwMU4xaitLY0RmRVcrR3FPZz09', NULL, NULL, NULL, NULL, NULL, NULL, '1', '2023-01-18 22:06:51', '2023-01-20 12:10:43'),
-(4, 'niQAouL4jM', 'F0oAeeh1Nr', '', 'phQBx224pb', 'l9CtLRZz5Q', '', 'ijSl4emsLS', 'LCJZPoAsTb', 'kth@prime.edu.pk', '', '$2y$10$Yi/XQdvqss4Rsq6/WnYXwePVEKjqM1wPAfutoRxUpwHlNjwiTwRKm', '949780', '855614', 206775, 'WVpmWDlQbTF0ZWpGMHJjMnZEcWk1V1p3Zy8vcDlzVzkxV09jVE4wRWpzVT0=', 'T1c1V0dJYXJkcVNlQXphNFdlZFFPQT09', 'QjlhMENHeURLbGl1T2h4ejF4cEt3QT09', 'MU5VdjZ4djkyS3FGS085ZHVmSFhMZz09', 'eElqQ29UQW4veUE2b0E2NEhlRmg0Zz09', 'U0gyQzhUUzVCNkppL2d5L3JDYzU3MjUrNUNZbWd1dDRnSDhKUkh3MFRIRWFLZ01lbFpNYzlNVXRNdEZuT2V3V2VvNFNXWE55WFRWWUlWQ01SMXpkb3c9PQ==', 'ejl3enJOeDFUMFhhb2FIbmR5eklnUT09', '1', '2023-01-18 22:48:11', '2023-01-20 12:13:37'),
-(5, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'mth@prime.edu.pk', '', '$2y$10$shMi9imDGi822cqijxVK3OOU9aokpowvNxTwnMKMuGMbxGJ73bzBi', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '0', '2023-01-18 22:52:54', '2023-01-20 12:15:04'),
-(6, 'KuNAVYBCYo', 'J50YD0Upsc', '', 'cyEAHyOfVs', 'MYbtDc7ite', '', 'zH32ITMbQT', 'UEz0fMBDTk', 'pth@prime.edu.pk', '', '$2y$10$amei78yjv1UfHIZNsZ08muNgOfQszsqSnodveQJf5X4wJ0kLjT26a', '872757', '6985934567', 215656, 'eElqQ29UQW4veUE2b0E2NEhlRmg0Zz09.jpg', 'NjJxQVhOQnpwNERFN3dseWJnN2pyZz09.jpg', 'eElqQ29UQW4veUE2b0E2NEhlRmg0Zz09.jpg', 'U0gyQzhUUzVCNkppL2d5L3JDYzU3MjUrNUNZbWd1dDRnSDhKUkh3MFRIRWFLZ01lbFpNYzlNVXRNdEZuT2V3V2VvNFNXWE55WFRWWUlWQ01SMXpkb3c9PQ==.jpg', 'ejl3enJOeDFUMFhhb2FIbmR5eklnUT09.jpg', 'WVZ4UGJ6UTlJZXByZURSTFRNMlRUaDhGbkM3QU5RWVVNSDQzWjBGZGhzakpTWDlKdVBGN0xaN0ZUWjlRd2ozdA==.jpg', 'WVpmWDlQbTF0ZWpGMHJjMnZEcWk1V1p3Zy8vcDlzVzkxV09jVE4wRWpzVT0=.jpg', '0', '2023-01-18 22:58:38', '2023-01-20 12:13:42'),
-(7, '2', '5', '4', '3', '5', '2', 'okashaaa', 'ok', 'ok@gmail.com', '', 'ok@gmail.com', '786543', '67897654', 4567890, 'V3cvYlVqOVhwdm9IRHdVTllhWGd1ZHF4SDZueHVCWXlqVzFSaEI4cHVDbz0=.pdf', 'V3cvYlVqOVhwdm9IRHdVTllhWGd1ZHF4SDZueHVCWXlqVzFSaEI4cHVDbz0=.pdf', 'V3cvYlVqOVhwdm9IRHdVTllhWGd1ZHF4SDZueHVCWXlqVzFSaEI4cHVDbz0=.pdf', 'V3cvYlVqOVhwdm9IRHdVTllhWGd1ZHF4SDZueHVCWXlqVzFSaEI4cHVDbz0=.pdf', 'V3cvYlVqOVhwdm9IRHdVTllhWGd1ZHF4SDZueHVCWXlqVzFSaEI4cHVDbz0=.pdf', 'V3cvYlVqOVhwdm9IRHdVTllhWGd1ZHF4SDZueHVCWXlqVzFSaEI4cHVDbz0=.pdf', 'V3cvYlVqOVhwdm9IRHdVTllhWGd1ZHF4SDZueHVCWXlqVzFSaEI4cHVDbz0=.pdf', '1', '2023-01-24 12:27:19', '2023-01-24 12:28:29');
+(1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'shahid@prime.edu.pk', 'F', '$2y$10$r/.rBm5s1H8g5uR9KeJkS.ryJo6EU.G6Wm3f6xbEoxMkZdtDdobbG', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '1', '2023-01-18 09:58:07', '2023-01-25 13:11:38'),
+(2, '1', '1', '3', '1', '1', '4', 'abc', 'prime doc', 'okasha@gmail.com', 'M', '$2y$10$J9x2v52mwYt/8aKzDFy3wOHY3qsc7bWkQGV/JZcJWq/.ySaJmyfwi', '924524', '714057', 725133, 'TmVEMytzYzJqK05yUWp3N3VzazZkVFdiOEFNSy9GSXRSbWVOSlBYZW5XOD0=', 'ZzE4TGVHYkFNc1o1NlVjQmJadkMzUG5SeVdwMkZOTlhyR2EvNUxZQTZuUT0=', 'VlY1RFpjOFZnSzNEMmZRRE8zb3hhVmVCbStva25FLy9Cc0dqeWtLVFRDQT0=', 'ZzE4TGVHYkFNc1o1NlVjQmJadkMzTjAvekFoRElhYjVDNnFqUllKQXFmWT0=', 'QjlhMENHeURLbGl1T2h4ejF4cEt3QT09', 'QjlhMENHeURLbGl1T2h4ejF4cEt3QT09', 'QjlhMENHeURLbGl1T2h4ejF4cEt3QT09', '1', '2023-01-18 11:35:25', '2023-02-02 11:52:17'),
+(3, 'yJUX7csFaF', 'zcSRZKFIYv', '', 'myKnyGx0l8', 'AKGMjse3UN', '', 'qeq8aHtKCC', 'gRDsECihGB', 'test@pmc.com', 'F', '$2y$10$m96xc1NSjcLyD0b...VoT.BzXdmJTbhjr.E4/ajbv6R7V92.ptKty', '533106', '224990', 132701, 'bW9OMGIwMU4xaitLY0RmRVcrR3FPZz09', NULL, NULL, NULL, NULL, NULL, NULL, '1', '2023-01-18 22:06:51', '2023-01-20 12:10:43'),
+(4, 'niQAouL4jM', 'F0oAeeh1Nr', '', 'phQBx224pb', 'l9CtLRZz5Q', '', 'ijSl4emsLS', 'LCJZPoAsTb', 'kth@prime.edu.pk', 'M', '$2y$10$Yi/XQdvqss4Rsq6/WnYXwePVEKjqM1wPAfutoRxUpwHlNjwiTwRKm', '949780', '855614', 206775, 'WVpmWDlQbTF0ZWpGMHJjMnZEcWk1V1p3Zy8vcDlzVzkxV09jVE4wRWpzVT0=', 'T1c1V0dJYXJkcVNlQXphNFdlZFFPQT09', 'QjlhMENHeURLbGl1T2h4ejF4cEt3QT09', 'MU5VdjZ4djkyS3FGS085ZHVmSFhMZz09', 'eElqQ29UQW4veUE2b0E2NEhlRmg0Zz09', 'U0gyQzhUUzVCNkppL2d5L3JDYzU3MjUrNUNZbWd1dDRnSDhKUkh3MFRIRWFLZ01lbFpNYzlNVXRNdEZuT2V3V2VvNFNXWE55WFRWWUlWQ01SMXpkb3c9PQ==', 'ejl3enJOeDFUMFhhb2FIbmR5eklnUT09', '1', '2023-01-18 22:48:11', '2023-01-20 12:13:37'),
+(5, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'mth@prime.edu.pk', 'M', '$2y$10$shMi9imDGi822cqijxVK3OOU9aokpowvNxTwnMKMuGMbxGJ73bzBi', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '0', '2023-01-18 22:52:54', '2023-01-20 12:15:04'),
+(6, 'KuNAVYBCYo', 'J50YD0Upsc', '', 'cyEAHyOfVs', 'MYbtDc7ite', '', 'zH32ITMbQT', 'UEz0fMBDTk', 'pth@prime.edu.pk', 'F', '$2y$10$amei78yjv1UfHIZNsZ08muNgOfQszsqSnodveQJf5X4wJ0kLjT26a', '872757', '6985934567', 215656, 'eElqQ29UQW4veUE2b0E2NEhlRmg0Zz09.jpg', 'NjJxQVhOQnpwNERFN3dseWJnN2pyZz09.jpg', 'eElqQ29UQW4veUE2b0E2NEhlRmg0Zz09.jpg', 'U0gyQzhUUzVCNkppL2d5L3JDYzU3MjUrNUNZbWd1dDRnSDhKUkh3MFRIRWFLZ01lbFpNYzlNVXRNdEZuT2V3V2VvNFNXWE55WFRWWUlWQ01SMXpkb3c9PQ==.jpg', 'ejl3enJOeDFUMFhhb2FIbmR5eklnUT09.jpg', 'WVZ4UGJ6UTlJZXByZURSTFRNMlRUaDhGbkM3QU5RWVVNSDQzWjBGZGhzakpTWDlKdVBGN0xaN0ZUWjlRd2ozdA==.jpg', 'WVpmWDlQbTF0ZWpGMHJjMnZEcWk1V1p3Zy8vcDlzVzkxV09jVE4wRWpzVT0=.jpg', '0', '2023-01-18 22:58:38', '2023-01-20 12:13:42'),
+(7, '2', '5', '4', '3', '5', '2', 'okashaaa', 'ok', 'ok@gmail.com', 'M', 'ok@gmail.com', '786543', '67897654', 4567890, 'V3cvYlVqOVhwdm9IRHdVTllhWGd1ZHF4SDZueHVCWXlqVzFSaEI4cHVDbz0=.pdf', 'V3cvYlVqOVhwdm9IRHdVTllhWGd1ZHF4SDZueHVCWXlqVzFSaEI4cHVDbz0=.pdf', 'V3cvYlVqOVhwdm9IRHdVTllhWGd1ZHF4SDZueHVCWXlqVzFSaEI4cHVDbz0=.pdf', 'V3cvYlVqOVhwdm9IRHdVTllhWGd1ZHF4SDZueHVCWXlqVzFSaEI4cHVDbz0=.pdf', 'V3cvYlVqOVhwdm9IRHdVTllhWGd1ZHF4SDZueHVCWXlqVzFSaEI4cHVDbz0=.pdf', 'V3cvYlVqOVhwdm9IRHdVTllhWGd1ZHF4SDZueHVCWXlqVzFSaEI4cHVDbz0=.pdf', 'V3cvYlVqOVhwdm9IRHdVTllhWGd1ZHF4SDZueHVCWXlqVzFSaEI4cHVDbz0=.pdf', '1', '2023-01-24 12:27:19', '2023-01-24 12:28:29');
 
 -- --------------------------------------------------------
 
@@ -239,6 +239,8 @@ CREATE TABLE IF NOT EXISTS `orders` (
   `ord_assignment` varchar(255) DEFAULT NULL,
   `ord_comment2` varchar(1500) DEFAULT NULL,
   `ord_status` varchar(255) NOT NULL,
+  `ord_cancel_bdr` varchar(155) DEFAULT NULL COMMENT '1 for cancel or blank/null ',
+  `ord_cancel_bcl` varchar(155) DEFAULT NULL COMMENT '1 for cancel or blank/null',
   `ord_created` datetime DEFAULT NULL,
   `ord_updated` datetime DEFAULT NULL,
   PRIMARY KEY (`ord_id`)
@@ -248,10 +250,10 @@ CREATE TABLE IF NOT EXISTS `orders` (
 -- Dumping data for table `orders`
 --
 
-INSERT INTO `orders` (`ord_id`, `ord_L_S_date`, `ord_L_E_date`, `ord_speciality`, `ord_grade`, `cl_id`, `emp_id`, `ord_required_from`, `ord_required_to`, `ord_process_date`, `ord_process_details_from`, `ord_process_details_to`, `ord_confirmation_date`, `ord_invoice_id`, `ord_normal_hrs`, `ord_on_call_hrs`, `ord_total_hrs`, `ord_approx_cost`, `ord_pay_to_dr`, `ord_admin_charges`, `ord_diff_profit_admin`, `ord_time_sheet_rcvd`, `ord_time_sheet_mode`, `ord_time_sheet_process`, `ord_time_sheet_approved`, `ord_comment1`, `ord_invoice_refer`, `ord_invoice_date`, `ord_invoice_by`, `ord_sage_refer_no`, `ord_paymnt_rcvd_date`, `ord_pay_to_dr_date`, `ord_case_status`, `ord_payment_status`, `ord_assignment`, `ord_comment2`, `ord_status`, `ord_created`, `ord_updated`) VALUES
-(1, '2023-01-28 10:25:00', '2023-01-11 10:25:00', '2', '5', 2, 4, '2023-02-09 03:22:00', '2023-02-03 17:14:00', '2023-02-03', '2023-01-27 10:25:00', '2023-01-26 17:08:00', '2023-01-23', '23456', '15', '5', '20', '7', '34', '5', '6', '2023-01-25', 'whatsapp', '2023-01-19', 'Not-Approved', '123', '435325', '2023-01-09', 'ak', '2023-01-22', '2023-01-11', '2023-01-17', 'Pending', 'Pending', 'RWlxOEpoOW0ydkhJNmIya3pjYUo1UT09.pdf', 'abc', '1', '2023-01-23 10:25:20', '2023-01-26 23:59:31'),
-(2, '2023-01-28 10:25:00', '2023-01-11 10:25:00', '15', '2', 1, 3, '2023-02-09 03:22:00', '2023-01-31 10:27:00', '2023-02-03', '2023-01-27 10:25:00', '2023-01-23 10:25:00', '2023-01-23', '23456', '7', '5', '11', '7', '34', '5', '6', '2023-02-02', 'whatsapp', '2023-01-19', 'Approved', '324423', '435325', '2023-01-09', 'ak', '2023-01-22', '2023-01-11', '2023-01-17', 'Closed', 'Paid', '', '', '3', '2023-01-23 12:15:57', '2023-01-26 13:50:41'),
-(3, '2023-01-06 10:32:00', '2023-01-17 10:32:00', '2', '3', 1, 4, '2023-01-31 10:32:00', '2023-01-28 00:32:00', '2023-01-20', '2023-01-27 10:32:00', '2023-01-20 10:32:00', '2023-01-25', 'SqQdIFe2z5', '999524', '585893', '912231', 'TpXC5LZ3Fv', 'Tg9OfM6DX7', 'Hjpmt6MAd1', 'WhfUQc6CG2', '2023-01-17', 'email', '2023-01-10', 'Approved', 'jpKJczGQRC', '9hHD6FSisw', '2023-01-16', 'I6ux4dd5gF', '2023-01-19', '2023-01-27', '2023-01-20', 'Closed', 'Paid', '', 'Z75fbsji54', '1', '2023-01-26 17:49:34', '2023-01-26 17:55:55');
+INSERT INTO `orders` (`ord_id`, `ord_L_S_date`, `ord_L_E_date`, `ord_speciality`, `ord_grade`, `cl_id`, `emp_id`, `ord_required_from`, `ord_required_to`, `ord_process_date`, `ord_process_details_from`, `ord_process_details_to`, `ord_confirmation_date`, `ord_invoice_id`, `ord_normal_hrs`, `ord_on_call_hrs`, `ord_total_hrs`, `ord_approx_cost`, `ord_pay_to_dr`, `ord_admin_charges`, `ord_diff_profit_admin`, `ord_time_sheet_rcvd`, `ord_time_sheet_mode`, `ord_time_sheet_process`, `ord_time_sheet_approved`, `ord_comment1`, `ord_invoice_refer`, `ord_invoice_date`, `ord_invoice_by`, `ord_sage_refer_no`, `ord_paymnt_rcvd_date`, `ord_pay_to_dr_date`, `ord_case_status`, `ord_payment_status`, `ord_assignment`, `ord_comment2`, `ord_status`, `ord_cancel_bdr`, `ord_cancel_bcl`, `ord_created`, `ord_updated`) VALUES
+(1, '2023-01-28 10:25:00', '2023-01-11 10:25:00', '2', '5', 2, 2, '2023-02-09 03:22:00', '2023-02-09 17:14:00', '2023-02-03', '2023-01-27 10:25:00', '2023-01-26 17:08:00', '2023-01-23', '23456', '15', '5', '20', '7', '34', '9', '6', '2023-01-25', 'whatsapp', '2023-01-19', 'Not-Approved', '123', '435325', '2023-01-09', 'ak', '2023-01-22', '2023-01-11', '2023-01-17', 'Pending', 'Pending', 'T3FiMUtuMmd6R2Fjc0tYa0lLYlVkcVJ3SkF3YkdZOWxPdVBkU0NyVjY5WT0=.pdf', 'abc', '3', '1', '', '2023-01-23 10:25:20', '2023-02-03 12:45:16'),
+(2, '2023-01-28 10:25:00', '2023-01-11 10:25:00', '15', '2', 1, 3, '2023-02-09 03:22:00', '2023-02-18 10:27:00', '2023-02-03', '2023-01-27 10:25:00', '2023-01-23 10:25:00', '2023-01-23', '23456', '7', '5', '11', '7', '34', '5', '6', '2023-02-02', 'whatsapp', '2023-01-19', 'Approved', '324423', '435325', '2023-01-09', 'ak', '2023-01-22', '2023-01-11', '2023-01-17', 'Closed', 'Paid', '', '', '3', NULL, NULL, '2023-01-23 12:15:57', '2023-02-02 13:08:32'),
+(3, '2023-01-06 10:32:00', '2023-01-17 10:32:00', '2', '3', 1, 2, '2023-01-31 10:32:00', '2023-02-23 00:32:00', '2023-01-20', '2023-01-27 10:32:00', '2023-01-20 10:32:00', '2023-01-25', 'SqQdIFe2z5', '999524', '585893', '912231', 'TpXC5LZ3Fv', 'Tg9OfM6DX7', 'Hjpmt6MAd1', 'WhfUQc6CG2', '2023-01-17', 'email', '2023-01-10', 'Approved', 'jpKJczGQRC', '9hHD6FSisw', '2023-01-16', 'I6ux4dd5gF', '2023-01-19', '2023-01-27', '2023-01-20', 'Closed', 'Paid', '', 'Z75fbsji54', '1', '', NULL, '2023-01-26 17:49:34', '2023-02-03 10:28:34');
 
 -- --------------------------------------------------------
 
